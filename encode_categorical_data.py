ac64533f95e1157dbdfb829be77a94a4a8c8f379
taru <kirikia@gmail.com>
2023-11-11 00:35:54 +0900

    使いやすいtarget encoding classを追加

--- encode_categorical_data.py
+++ encode_categorical_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn import base
 from sklearn.model_selection import KFold
 
 
@@ -42,44 +43,78 @@ def test_target_encoding():
     train_x = pd.DataFrame({'cat_var': np.random.choice(['A', 'B', 'C'], size=20)})
     train_y = np.random.rand(20) > 0.5  # ランダムなバイナリターゲット
     test_x = pd.DataFrame({'cat_var': np.random.choice(['A', 'B', 'C'], size=10)})
-    cat_cols = ['cat_var']
 
-    # target_encoding関数をテスト
-    train_x_encoded = target_encoding(train_x.copy(), train_y, test_x.copy(), cat_cols)
+    X = pd.concat([train_x, pd.DataFrame(train_y, columns=['target'])], axis=1)
+    targetc = KFoldTargetEncoderTrain('cat_var', 'target')
+    new_train = targetc.fit_transform(X)
+    print(new_train)
 
-    print("Original Train X:\n", train_x)
-    print("\nEncoded Train X:\n", train_x_encoded)
-    print("\nOriginal Test X:\n", test_x)
+    test_targetc = KFoldTargetEncoderTest(new_train, 'cat_var', 'cat_var_Kfold_Target_Enc')
+    new_test = test_targetc.fit_transform(test_x)
+    print(new_test)
 
 
-def target_encoding_cv(train_x, train_y, test_x, cat_cols):
-    # クロスバリデーションのfoldごとにtarget encodingをやり直す
-    kf = KFold(n_splits=4, shuffle=True, random_state=42)
-    for i, (tr_idx, va_idx) in enumerate(kf.split(train_x)):
-        # 学習データからバリデーションデータを分ける
-        tr_x, va_x = train_x.iloc[tr_idx].copy(), train_x.iloc[va_idx].copy()
-        tr_y, va_y = train_y.iloc[tr_idx], train_y.iloc[va_idx]
+class KFoldTargetEncoderTrain(base.BaseEstimator, base.TransformerMixin):
+    """
+    参考notebook：
+    https://www.kaggle.com/code/anuragbantu/target-encoding-beginner-s-guide
+    """
 
-        # 変数をループしてtarget encoding
-        for c in cat_cols:
-            # 学習データ全体で各カテゴリにおけるtargetの平均を計算
-            data_tmp = pd.DataFrame({c: tr_x[c], 'target': tr_y})
-            target_mean = data_tmp.groupby(c)['target'].mean()
-            # バリデーションデータのカテゴリを置換
-            va_x.loc[:, c] = va_x[c].map(target_mean)
+    def __init__(self, colnames, targetName, n_fold=5, verbosity=True, discardOriginal_col=False):
+        self.colnames = colnames
+        self.targetName = targetName
+        self.n_fold = n_fold
+        self.verbosity = verbosity
+        self.discardOriginal_col = discardOriginal_col
 
-            # 学習データの変換後の値を格納する配列を準備
-            tmp = np.repeat(np.nan, tr_x.shape[0])
-            kf_encoding = KFold(n_splits=4, shuffle=True, random_state=42)
-            for idx_1, idx_2 in kf_encoding.split(tr_x):
-                # one-of-fold で各カテゴリにおける目的変数の平均を計算
-                target_mean = data_tmp.iloc[idx_1].groupby(c)['target'].mean()
-                # 変換後の値を一時配列に格納
-                tmp[idx_2] = tr_x[c].iloc[idx_2].map(target_mean)
+    def fit(self, X, y=None):
+        return self
 
-            tr_x.loc[:, c] = tmp
+    def transform(self, X):
+        assert (isinstance(self.targetName, str))
+        assert (isinstance(self.colnames, str))
+        assert (self.colnames in X.columns)
+        assert (self.targetName in X.columns)
+        mean_of_target = X[self.targetName].mean()
+        kf = KFold(n_splits=self.n_fold, shuffle=True, random_state=2019)
+        col_mean_name = self.colnames + '_' + 'Kfold_Target_Enc'
+        X[col_mean_name] = np.nan
+        for tr_ind, val_ind in kf.split(X):
+            X_tr, X_val = X.iloc[tr_ind], X.iloc[val_ind]
+            X.loc[X.index[val_ind], col_mean_name] = X_val[self.colnames].map(X_tr.groupby(self.colnames)[self.targetName].mean())
+            X[col_mean_name].fillna(mean_of_target, inplace=True)
+        if self.verbosity:
+            encoded_feature = X[col_mean_name].values
+            print('Correlation between the new feature, {} and , {} is {}.'.format(col_mean_name,
+                  self.targetName, np.corrcoef(X[self.targetName].values, encoded_feature)[0][1]))
+        if self.discardOriginal_col:
+            X = X.drop(self.targetName, axis=1)
+        return X
 
-            # 必要に応じてencodeされた特徴量を保存し、あとで読み込めるようにしておく
+
+class KFoldTargetEncoderTest(base.BaseEstimator, base.TransformerMixin):
+    """
+    参考notebook：
+    https://www.kaggle.com/code/anuragbantu/target-encoding-beginner-s-guide
+    """
+
+    def __init__(self, train, colNames, encodedName):
+        self.train = train
+        self.colNames = colNames
+        self.encodedName = encodedName
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        mean = self.train[[self.colNames, self.encodedName]].groupby(self.colNames).mean().reset_index()
+
+        dd = {}
+        for index, row in mean.iterrows():
+            dd[row[self.colNames]] = row[self.encodedName]
+        X[self.encodedName] = X[self.colNames]
+        X = X.replace({self.encodedName: dd})
+        return X
 
 
 if __name__ == '__main__':
