4c182dba5e1e7393494e23cb94ca7692ecc0d4d6
taru <kirikia@gmail.com>
2023-11-27 00:17:31 +0900

    seabornを使ってプロットするように修正

--- eda/outlier_detection.py
+++ eda/outlier_detection.py
@@ -1,18 +1,12 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-
-def detect_outliers(df):
-    outliers = pd.DataFrame(columns=df.columns)
-    for col in df.columns:
-        if df[col].dtype == 'float64' or df[col].dtype == 'int64':
-            mean = df[col].mean()
-            std = df[col].std()
-            threshold = std * 3
-            outliers[col] = df[col].apply(lambda x: x if abs(x - mean) > threshold else None)
-    return outliers
 
 def detect_outliers_with_iqr(df):
     outliers = pd.DataFrame(columns=df.columns)
+    outlier_indices = []
+
     for col in df.columns:
         if df[col].dtype == 'float64' or df[col].dtype == 'int64':
             Q1 = df[col].quantile(0.25)
@@ -20,5 +14,21 @@ def detect_outliers_with_iqr(df):
             IQR = Q3 - Q1
             lower_bound = Q1 - 1.5 * IQR
             upper_bound = Q3 + 1.5 * IQR
-            outliers[col] = df[col].apply(lambda x: x if x < lower_bound or x > upper_bound else None)
-    return outliers
+
+            # 異常値のフラグを立てる
+            outlier_flag = df[col].apply(lambda x: x < lower_bound or x > upper_bound)
+            outliers[col] = df[col].where(outlier_flag, None)
+
+            # 異常値のインデックスを収集
+            outlier_indices.extend(df[col][outlier_flag].index.tolist())
+
+    # Seabornを使ったボックスプロットの描画
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(12, 8))
+    sns.boxplot(data=df, palette="Set2")
+    plt.title("Box Plot for Detecting Outliers with Seaborn")
+    plt.show()
+
+    # 重複を除いて異常値のインデックスを返す
+    unique_outlier_indices = list(set(outlier_indices))
+    return outliers, unique_outlier_indices
--- tests/eda/test_outlier_detection.py
+++ tests/eda/test_outlier_detection.py
@@ -11,25 +11,24 @@ from eda.outlier_detection import detect_outliers_with_iqr
 
 
 class TestDetectOutliersWithIQR(unittest.TestCase):
+
     def setUp(self):
         self.df = pd.DataFrame({
-            'col1': [1, 2, 3, 4, 100],  # 100は異常値
+            'col1': [1, 2, 3, 4, 100],
             'col2': [2, 4, 6, 8, 10],
             'col3': [5, 10, 15, 20, 25]
         })
 
     def test_outliers_detection(self):
-        # 異常値検出関数のテスト
-        result = detect_outliers_with_iqr(self.df)
-        self.assertTrue(result.at[4, 'col1'] == 100)  # col1の異常値をチェック
-        self.assertTrue(pd.isna(result.at[3, 'col1']))  # 非異常値がNaNであることを確認
+        outliers, indices = detect_outliers_with_iqr(self.df)
+        self.assertTrue(outliers['col1'].iloc[4] == 100)
+        self.assertIn(4, indices)
 
     def test_no_outliers(self):
-        # 異常値がないケースのテスト
         df_no_outliers = pd.DataFrame({'col1': [1, 2, 3, 4, 5]})
-        result = detect_outliers_with_iqr(df_no_outliers)
-        self.assertTrue(result['col1'].isnull().all())  # すべての値がNoneであることを確認
+        outliers, indices = detect_outliers_with_iqr(df_no_outliers)
+        self.assertTrue(outliers['col1'].isnull().all())
+        self.assertEqual(len(indices), 0)
 
-# テストの実行
 if __name__ == '__main__':
     unittest.main()
